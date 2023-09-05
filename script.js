9dab50f24ef3ffd681277696ea0f38cfb530fb8a
sindhya230404 <143774711+sindhya230404@users.noreply.github.com>
2023-09-05 19:16:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulb_on(){
+    
+    document.body.style.backgroundImage = "url('lightroom.avif')";
+    
+}
+function bulb_off(){
+    
+    document.body.style.backgroundImage = "url('Darkroom.avif')";
+}
